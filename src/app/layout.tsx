54fd6b99b4bd56264e5b29ba3f390997f12d918a
SAAD <saadaithammou05062003@gmail.com>
2026-03-11 16:35:01 +0200

    remove stars background from all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import AuthProvider from "./SessionProvider";
-import StarsBackground from "@/components/ui/StarsBackground";
 import Navbar from "@/components/layout/Navbar";
 import ThemeRouteClass from "@/components/ui/ThemeRouteClass";
 import { Web3Provider } from "@/components/ui/Web3Provider";
@@ -11,7 +10,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <AuthProvider>
           <Web3Provider>
             <ThemeRouteClass />
-            <StarsBackground />
             <Navbar />
             {children}
           </Web3Provider>
